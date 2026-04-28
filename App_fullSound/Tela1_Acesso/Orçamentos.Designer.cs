aaa9ded3d088e317f1b338d6526f203fc4838973
Isabela <isabela.marques39@gmail.com>
2026-04-27 22:00:18 -0300

    Orçamento

--- App_fullSound/Tela1_Acesso/Orçamentos.Designer.cs
+++ App_fullSound/Tela1_Acesso/Orçamentos.Designer.cs
@@ -45,6 +45,17 @@
             pictureBox17 = new PictureBox();
             pictureBox6 = new PictureBox();
             panel1 = new Panel();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            pictureBox7 = new PictureBox();
+            label8 = new Label();
+            label9 = new Label();
+            pictureBox8 = new PictureBox();
+            label12 = new Label();
+            panel2 = new Panel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -55,6 +66,10 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox16).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox17).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // lblHome
@@ -128,7 +143,7 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ButtonHighlight;
-            label2.Location = new Point(238, 24);
+            label2.Location = new Point(234, 24);
             label2.Name = "label2";
             label2.Size = new Size(126, 30);
             label2.TabIndex = 49;
@@ -168,25 +183,25 @@
             pictureBox5.BackColor = Color.Transparent;
             pictureBox5.BackgroundImage = (Image)resources.GetObject("pictureBox5.BackgroundImage");
             pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
-            pictureBox5.Location = new Point(12, 190);
+            pictureBox5.Location = new Point(12, 193);
             pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(25, 28);
+            pictureBox5.Size = new Size(25, 23);
             pictureBox5.TabIndex = 53;
             pictureBox5.TabStop = false;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(8, 225);
+            dataGridView1.Location = new Point(209, 232);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(661, 181);
+            dataGridView1.Size = new Size(460, 154);
             dataGridView1.TabIndex = 66;
             // 
             // pictureBox15
             // 
             pictureBox15.BackColor = Color.Transparent;
             pictureBox15.Image = (Image)resources.GetObject("pictureBox15.Image");
-            pictureBox15.Location = new Point(215, 24);
+            pictureBox15.Location = new Point(209, 25);
             pictureBox15.Name = "pictureBox15";
             pictureBox15.Size = new Size(26, 30);
             pictureBox15.TabIndex = 69;
@@ -195,9 +210,9 @@
             // pictureBox16
             // 
             pictureBox16.Image = (Image)resources.GetObject("pictureBox16.Image");
-            pictureBox16.Location = new Point(-4, 188);
+            pictureBox16.Location = new Point(-4, 190);
             pictureBox16.Name = "pictureBox16";
-            pictureBox16.Size = new Size(166, 29);
+            pictureBox16.Size = new Size(166, 28);
             pictureBox16.TabIndex = 70;
             pictureBox16.TabStop = false;
             // 
@@ -205,34 +220,173 @@
             // 
             pictureBox17.BackgroundImage = (Image)resources.GetObject("pictureBox17.BackgroundImage");
             pictureBox17.Image = (Image)resources.GetObject("pictureBox17.Image");
-            pictureBox17.Location = new Point(1, 189);
+            pictureBox17.Location = new Point(1, 190);
             pictureBox17.Name = "pictureBox17";
-            pictureBox17.Size = new Size(10, 28);
+            pictureBox17.Size = new Size(10, 27);
             pictureBox17.TabIndex = 71;
             pictureBox17.TabStop = false;
             // 
             // pictureBox6
             // 
             pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
-            pictureBox6.Location = new Point(446, 414);
+            pictureBox6.Location = new Point(452, 408);
             pictureBox6.Name = "pictureBox6";
-            pictureBox6.Size = new Size(172, 42);
+            pictureBox6.Size = new Size(226, 48);
             pictureBox6.TabIndex = 72;
             pictureBox6.TabStop = false;
             // 
             // panel1
             // 
-            panel1.Location = new Point(209, 95);
+            panel1.BackColor = Color.WhiteSmoke;
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(211, 84);
             panel1.Name = "panel1";
-            panel1.Size = new Size(423, 100);
+            panel1.Size = new Size(458, 99);
             panel1.TabIndex = 73;
+            panel1.Paint += panel1_Paint;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.MediumSeaGreen;
+            label7.Image = (Image)resources.GetObject("label7.Image");
+            label7.Location = new Point(10, 47);
+            label7.Name = "label7";
+            label7.Size = new Size(33, 21);
+            label7.TabIndex = 4;
+            label7.Text = "R$ ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Image = (Image)resources.GetObject("label6.Image");
+            label6.Location = new Point(319, 49);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 21);
+            label6.TabIndex = 3;
+            label6.Text = "Saldo: R$";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Image = (Image)resources.GetObject("label5.Image");
+            label5.Location = new Point(219, 49);
+            label5.Name = "label5";
+            label5.Size = new Size(29, 21);
+            label5.TabIndex = 2;
+            label5.Text = "R$";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Image = (Image)resources.GetObject("label4.Image");
+            label4.Location = new Point(113, 48);
+            label4.Name = "label4";
+            label4.Size = new Size(29, 21);
+            label4.TabIndex = 1;
+            label4.Text = "R$";
+            label4.Click += label4_Click_1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Image = (Image)resources.GetObject("label3.Image");
+            label3.Location = new Point(10, 16);
+            label3.Name = "label3";
+            label3.Size = new Size(109, 21);
+            label3.TabIndex = 0;
+            label3.Text = "Fluxo de Caixa";
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.Violet;
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(387, 321);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(32, 32);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 74;
+            pictureBox7.TabStop = false;
+            pictureBox7.Click += pictureBox7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(211, 201);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 15);
+            label8.TabIndex = 75;
+            label8.Text = "label8";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.ForeColor = SystemColors.ButtonHighlight;
+            label9.Location = new Point(210, 208);
+            label9.Name = "label9";
+            label9.Size = new Size(111, 17);
+            label9.TabIndex = 76;
+            label9.Text = "Listas de Serviços";
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.FromArgb(245, 166, 35);
+            pictureBox8.BackgroundImageLayout = ImageLayout.None;
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(5, 5);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(24, 24);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 77;
+            pictureBox8.TabStop = false;
+            pictureBox8.Click += pictureBox8_Click;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.DarkOrange;
+            label12.Image = (Image)resources.GetObject("label12.Image");
+            label12.Location = new Point(506, 422);
+            label12.Name = "label12";
+            label12.Size = new Size(124, 17);
+            label12.TabIndex = 78;
+            label12.Text = "Emitir Comprovante";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox8);
+            panel2.Location = new Point(463, 415);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(32, 32);
+            panel2.TabIndex = 79;
             // 
             // Orçamentos
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
-            ClientSize = new Size(675, 468);
+            ClientSize = new Size(707, 468);
+            Controls.Add(panel2);
+            Controls.Add(label12);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(pictureBox7);
             Controls.Add(panel1);
             Controls.Add(pictureBox6);
             Controls.Add(pictureBox17);
@@ -251,6 +405,7 @@
             Controls.Add(pictureBox16);
             Name = "Orçamentos";
             Text = "Orçamentos";
+            Load += Orçamentos_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
@@ -261,6 +416,11 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox16).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox17).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -312,5 +472,14 @@
         private PictureBox pictureBox17;
         private PictureBox pictureBox6;
         private Panel panel1;
+        private PictureBox pictureBox7;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private PictureBox pictureBox8;
+        private Label label12;
+        private Panel panel2;
     }
 }
--- App_fullSound/Tela1_Acesso/Orçamentos.cs
+++ App_fullSound/Tela1_Acesso/Orçamentos.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace Tela1_Acesso
 {
@@ -122,7 +124,58 @@ namespace Tela1_Acesso
         {
 
         }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            using (var imgTemp = Image.FromFile(@"C:\caminho\icone.png"))
+            {
+                pictureBox8.Image = new Bitmap(imgTemp);
+            }
+
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.BackColor = Color.Transparent;
+
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void ArredondarPanel(Panel panel, int raio)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+
+            path.AddArc(0, 0, raio, raio, 180, 90);
+            path.AddArc(panel.Width - raio, 0, raio, raio, 270, 90);
+            path.AddArc(panel.Width - raio, panel.Height - raio, raio, raio, 0, 90);
+            path.AddArc(0, panel.Height - raio, raio, raio, 90, 90);
+
+            path.CloseFigure();
+            panel.Region = new Region(path);
+        }
+
+        private void Orçamentos_Load(object sender, EventArgs e)
+        {
+            ArredondarPanel(panel1, 20);
+            ArredondarPanel(panel2, 20);
+        }
     }
+
 
 }
 
